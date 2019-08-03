f0fed5ba4c7826e8edad7e2bcdbffbca8b13067f
Jordan Mackie <jmackie97@hotmail.com>
2019-08-03 15:49:56 +0100

    configure next question

---
+++ src/test/MinCostMapColouringTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+public class MinCostMapColouringTest {
+    @Test
+    public void getMinimumCost() {
+    }
+}
