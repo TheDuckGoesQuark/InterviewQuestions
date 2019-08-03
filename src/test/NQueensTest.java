a94d51e021cddb3534775eb5ef210069f0dce432
Jordan Mackie <jmackie97@hotmail.com>
2019-08-03 17:05:48 +0100

    set up n queens problem

---
+++ src/test/NQueensTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NQueensTest {
+
+    @Test
+    public void getNumberOfPossibleArrangements() {
+    }
+}
