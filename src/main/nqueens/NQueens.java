023857bb57167c46ecd49d5c224b08eb0168a8c7
Jordan Mackie <jmackie97@hotmai.com>
2019-08-03 17:05:48 +0100

    set up n queens problem

---
+++ src/main/nqueens/NQueens.java
@@ -0,0 +1,16 @@
+package main.nqueens;
+
+/**
+ * You have an N by N board.
+ * <p>
+ * Write a function that returns the number of possible arrangements of the board
+ * where N queens can be placed on the board without threatening each other,
+ * i.e. no two queens share the same row, column, or diagonal.
+ */
+public class NQueens {
+
+    public static int getNumberOfPossibleArrangements(int boardSize, int nQueens) {
+        return 0;
+    }
+
+}
