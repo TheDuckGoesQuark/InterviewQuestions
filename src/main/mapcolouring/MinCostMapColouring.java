e7ada8617d6c9fbc5e5b9bb8f312db9621197cbd
Jordan Mackie <jmackie97@hotmai.com>
2019-08-03 15:49:56 +0100

    configure next question

---
+++ src/main/mapcolouring/MinCostMapColouring.java
@@ -0,0 +1,16 @@
+package main.mapcolouring;
+
+/**
+ * A builder is looking to build a row of N houses that can be of K different colors.
+ * He has a goal of minimizing cost while ensuring that no two neighboring houses are of the same color.
+ * <p>
+ * Given an N by K matrix where the nth row and kth column represents the cost to build the nth house with kth color,
+ * return the minimum cost which achieves this goal.
+ */
+public class MinCostMapColouring {
+
+    public static int getMinimumCost(int[][] houseColourCosts) {
+        return 0;
+    }
+
+}
