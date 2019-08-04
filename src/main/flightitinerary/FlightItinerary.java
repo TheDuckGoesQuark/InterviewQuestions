d3c69bb1fd651c13c0271e8e342b037e5d22dae4
Jordan Mackie <jmackie97@hotmai.com>
2019-08-04 19:10:45 +0100

    flight itinerary question passed

--- src/main/flightitinerary/FlightItinerary.java
+++ src/main/flightitinerary/FlightItinerary.java
@@ -1,6 +1,5 @@
 package main.flightitinerary;
 
-import javax.swing.text.html.Option;
 import java.util.*;
 
 /**
@@ -28,41 +27,52 @@ public class FlightItinerary {
      * or empty optional if no possible itinerary can be created
      */
     public static Optional<String[]> produceItinerary(String[][] flights, String homeAirport) {
-        return recursivelyFindItinerary(Arrays.asList(flights), new Stack<>(), homeAirport, homeAirport)
-                .map(strings -> (String[]) (strings.toArray()));
+        Optional<Stack<String[]>> result = recursivelyFindItinerary(new LinkedList<>(Arrays.asList(flights)), new Stack<>(), homeAirport);
+
+        if (result.isEmpty()) return Optional.empty();
+
+        Stack<String[]> flightStack = result.get();
+        String[] itinerary = new String[flightStack.size() + 1];
+
+        for (int i = flightStack.size(); i > 0; i--) {
+            itinerary[i] = flightStack.pop()[1];
+        }
+
+        itinerary[0] = homeAirport;
+
+        return Optional.of(itinerary);
     }
+
+    /**
+     * yul, ord, sfo, hnl, ask
+     */
 
     /**
      * @param flights     origin-destination pairs of airport names that can still be used
      * @param usedFlights flights used so far in itinerary
      * @param origin      airport that we must start from
-     * @param end         airport that we must return to eventually
      * @return a itinerary string array of each airport to stop at in order,
      * or empty optional if no possible itinerary can be created
      */
-    private static Optional<Stack<String[]>> recursivelyFindItinerary(List<String[]> flights, Stack<String[]> usedFlights, String origin, String end) {
-        // Base case: all flights consumed and we're back home!
-        if (flights.isEmpty() && origin.equals(end))
+    private static Optional<Stack<String[]>> recursivelyFindItinerary(LinkedList<String[]> flights, Stack<String[]> usedFlights, String origin) {
+        // Base case: all flights consumed
+        if (flights.isEmpty())
             return Optional.of(usedFlights);
-
-        // Base case: no way home left, not worth proceeding
-        if (flights.stream().noneMatch(originDestinationPair -> originDestinationPair[1].equals(end)))
-            return Optional.empty();
 
         // Try each next possible flight
         for (int i = 0; i < flights.size(); i++) {
             String[] flight = flights.remove(i);
+            usedFlights.push(flight);
 
             // If flight leaves from current airport
             if (flight[0].equals(origin)) {
-                usedFlights.push(flight);
-                Optional<Stack<String[]>> result = recursivelyFindItinerary(flights, usedFlights, flight[0], end);
+                Optional<Stack<String[]>> result = recursivelyFindItinerary(flights, usedFlights, flight[1]);
 
                 if (result.isPresent())
                     return result;
-                else
-                    flights.add(usedFlights.pop());
             }
+
+            flights.add(i, usedFlights.pop());
         }
 
         return Optional.empty();
