83f8733a7f3773b761f0cffab3ec96dd5e5bf39e
Jordan Mackie <jmackie97@hotmail.com>
2019-08-04 17:06:10 +0100

    add recursive solution for n queens

--- src/main/nqueens/NQueens.java
+++ src/main/nqueens/NQueens.java
@@ -9,8 +9,51 @@ package main.nqueens;
  */
 public class NQueens {
 
-    public static int getNumberOfPossibleArrangements(int boardSize, int nQueens) {
-        return 0;
+    public static int getNumberOfPossibleArrangements(int boardSize) {
+        return recursivelyGetNumberOfPossibleArrangements(new int[boardSize], 0);
     }
 
+    private static int recursivelyGetNumberOfPossibleArrangements(int[] currentQueens, int currentRow) {
+        if (currentRow == currentQueens.length) return 1; // all queens placed, return possible solution
+
+        int numberOfArrangements = 0;
+        // For each column, place the queen and find all possible arrangements for queens on lower rows
+        for (int currentColumn = 0; currentColumn < currentQueens.length; currentColumn++) {
+            if (canPlaceQueen(currentQueens, currentRow, currentRow, currentColumn)) {
+
+                currentQueens[currentRow] = currentColumn;
+
+                numberOfArrangements += recursivelyGetNumberOfPossibleArrangements(currentQueens, currentRow + 1);
+            }
+        }
+
+        return numberOfArrangements;
+    }
+
+    /**
+     * Returns true if the coordinates of the new queen do not result in a threat to
+     * an existing queen
+     *
+     * @param currentQueens x,y coordinates of each queen currently placed
+     * @param nQueens       number of queens currently placed
+     * @param newQueenX     x coordinate of new queen
+     * @param newQueenY     y coordinate of new queen
+     * @return true if the queen can be placed without threatening another queen
+     */
+    private static boolean canPlaceQueen(int[] currentQueens, int nQueens, int newQueenX, int newQueenY) {
+        for (int currentQueenX = 0; currentQueenX < nQueens; currentQueenX++) {
+            int currentQueenY = currentQueens[currentQueenX];
+
+            // Check horizontals
+            if (newQueenX == currentQueenX || newQueenY == currentQueenY)
+                return false;
+
+            // Check diagonals (y = mx + c, m = 1, on same diagonal if y1 (+/-) x1 == y2 (+/-) x2
+            if (newQueenY - newQueenX == currentQueenY - currentQueenX
+                    || newQueenY + newQueenX == currentQueenY + currentQueenX)
+                return false;
+        }
+
+        return true;
+    }
 }
