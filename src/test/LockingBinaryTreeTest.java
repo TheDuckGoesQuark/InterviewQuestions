41a08ef85ec86d20c2d1cd19d4b6803cfbae474a
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-07 10:27:29 +0100

    initial locking binary tree question

---
+++ src/test/LockingBinaryTreeTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LockingBinaryTreeTest {
+
+    @Test
+    public void is_locked() {
+    }
+
+    @Test
+    public void lock() {
+    }
+
+    @Test
+    public void unlock() {
+    }
+}
