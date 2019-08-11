d440c30561fd94b11e2e7002646741da857821c9
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-11 21:11:48 +0100

    balanced brackets done

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.balancedbrackets.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,5 +8,8 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
     @Test
     public void bracketsAreBalanced() {
+        assertTrue(BalancedBrackets.bracketsAreBalanced("([])[]({})"));
+        assertFalse(BalancedBrackets.bracketsAreBalanced("([)]"));
+        assertFalse(BalancedBrackets.bracketsAreBalanced("((()"));
     }
 }
