6fa972d495fab62677c96f3c6fec7acb467b77fe
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-11 19:28:42 +0100

    init balanced brackets test

---
+++ src/main/balancedbrackets/BalancedBrackets.java
@@ -0,0 +1,17 @@
+package main.balancedbrackets;
+
+/**
+ * Given a string of round, curly, and square open and closing brackets,
+ * return whether the brackets are balanced (well-formed).
+ * <p>
+ * For example, given the string "([])[]({})", you should return true.
+ * <p>
+ * Given the string "([)]" or "((()", you should return false.
+ */
+public class BalancedBrackets {
+
+    public static boolean bracketsAreBalanced(String str) {
+        return false;
+    }
+
+}
