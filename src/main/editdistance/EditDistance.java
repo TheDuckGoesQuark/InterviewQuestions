fae1980c48c8f8d0cff46194192ff0f763aac53f
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-12 16:19:57 +0100

    added DP to edit distance

--- src/main/editdistance/EditDistance.java
+++ src/main/editdistance/EditDistance.java
@@ -1,5 +1,9 @@
 package main.editdistance;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * The edit distance between two strings refers to
  * the minimum number of character insertions, deletions, and substitutions
@@ -13,7 +17,48 @@ package main.editdistance;
 public class EditDistance {
 
     public static int getEditDistance(String a, String b) {
-        return 0;
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        try {
+            return recursivelyGetEditDistance(a, b, a.length(), b.length(), map);
+        } finally {
+            System.out.println("");
+        }
     }
 
+    private static int recursivelyGetEditDistance(String a, String b, int lengthA, int lengthB, Map<Integer, Map<Integer, Integer>> alreadyTried) {
+        // if no more characters in string a, we need as many insertions as there are characters in string b
+        if (lengthA == 0) {
+            return lengthB;
+        }
+
+        // reverse of above
+        if (lengthB == 0) {
+            return lengthA;
+        }
+
+        // if characters are the same, then we don't need to do anything
+        if (a.charAt(lengthA - 1) == b.charAt(lengthB - 1))
+            return recursivelyGetEditDistance(a, b, lengthA - 1, lengthB - 1, alreadyTried);
+
+        if (!alreadyTried.containsKey(lengthA)) alreadyTried.put(lengthA, new HashMap<>());
+        // get insertion cost
+        Map<Integer, Integer> knownCosts = alreadyTried.get(lengthA);
+        if (!knownCosts.containsKey(lengthB - 1))
+            knownCosts.put(lengthB - 1, recursivelyGetEditDistance(a, b, lengthA, lengthB - 1, alreadyTried));
+        int insertion = knownCosts.get(lengthB - 1);
+
+        if (!alreadyTried.containsKey(lengthA - 1)) alreadyTried.put(lengthA - 1, new HashMap<>());
+
+        knownCosts = alreadyTried.get(lengthA - 1);
+        if (!knownCosts.containsKey(lengthB))
+            knownCosts.put(lengthB, recursivelyGetEditDistance(a, b, lengthA - 1, lengthB, alreadyTried));
+        int deletion = knownCosts.get(lengthB);
+
+        if (!knownCosts.containsKey(lengthB - 1))
+            knownCosts.put(lengthB - 1, recursivelyGetEditDistance(a, b, lengthA - 1, lengthB - 1, alreadyTried));
+
+        int substitution = knownCosts.get(lengthB - 1);
+
+        return 1 + Integer.min(Integer.min(insertion, deletion), substitution);
+    }
 }
--- src/test/EditDistanceTest.java
+++ src/test/EditDistanceTest.java
@@ -13,7 +13,7 @@ public class EditDistanceTest {
         assertEquals(1, EditDistance.getEditDistance("abcde", "abde"));
         assertEquals(1, EditDistance.getEditDistance("car", "far"));
         assertEquals(1, EditDistance.getEditDistance("islander", "slander"));
-        assertEquals(5, EditDistance.getEditDistance("mart", "karma"));
-        assertEquals(8, EditDistance.getEditDistance("intention", "execution"));
+        assertEquals(3, EditDistance.getEditDistance("mart", "karma"));
+        assertEquals(5, EditDistance.getEditDistance("intention", "execution"));
     }
 }
