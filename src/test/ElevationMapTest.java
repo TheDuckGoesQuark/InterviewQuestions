4bb5f33f4db2458f0f81407440716476dac54378
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-12 13:12:17 +0100

    init elevation map question|

---
+++ src/test/ElevationMapTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import main.elevationmap.ElevationMap;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ElevationMapTest {
+
+    @Test
+    public void getVolumeContainable() {
+        assertEquals(8, ElevationMap.getVolumeContainable(new int[]{3, 0, 1, 3, 0, 5}));
+        assertEquals(1, ElevationMap.getVolumeContainable(new int[]{2, 1, 2}));
+        assertEquals(0, ElevationMap.getVolumeContainable(new int[]{0, 1, 3}));
+        assertEquals(0, ElevationMap.getVolumeContainable(new int[]{5, 5, 5}));
+        assertEquals(5, ElevationMap.getVolumeContainable(new int[]{5, 0, 5}));
+        assertEquals(3, ElevationMap.getVolumeContainable(new int[]{3, 0, 5}));
+        assertEquals(3, ElevationMap.getVolumeContainable(new int[]{5, 0, 3}));
+        assertEquals(4, ElevationMap.getVolumeContainable(new int[]{5, 4, 3, 4, 5}));
+    }
+}
