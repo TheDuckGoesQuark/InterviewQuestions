21657e9c7aee33ec009b3dd0e7f11676d821cabd
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-12 14:01:04 +0100

    some passing tests

--- src/test/ElevationMapTest.java
+++ src/test/ElevationMapTest.java
@@ -9,13 +9,16 @@ public class ElevationMapTest {
 
     @Test
     public void getVolumeContainable() {
+        assertEquals(1, ElevationMap.getVolumeContainable(new int[]{1, 0, 1}));
+        assertEquals(5, ElevationMap.getVolumeContainable(new int[]{5, 0, 5}));
+        assertEquals(3, ElevationMap.getVolumeContainable(new int[]{3, 0, 5}));
+        assertEquals(3, ElevationMap.getVolumeContainable(new int[]{5, 0, 3}));
         assertEquals(8, ElevationMap.getVolumeContainable(new int[]{3, 0, 1, 3, 0, 5}));
         assertEquals(1, ElevationMap.getVolumeContainable(new int[]{2, 1, 2}));
         assertEquals(0, ElevationMap.getVolumeContainable(new int[]{0, 1, 3}));
         assertEquals(0, ElevationMap.getVolumeContainable(new int[]{5, 5, 5}));
-        assertEquals(5, ElevationMap.getVolumeContainable(new int[]{5, 0, 5}));
-        assertEquals(3, ElevationMap.getVolumeContainable(new int[]{3, 0, 5}));
-        assertEquals(3, ElevationMap.getVolumeContainable(new int[]{5, 0, 3}));
         assertEquals(4, ElevationMap.getVolumeContainable(new int[]{5, 4, 3, 4, 5}));
+        assertEquals(8, ElevationMap.getVolumeContainable(new int[]{5, 4, 3, 4, 5, 4, 3, 4, 5}));
+        assertEquals(1, ElevationMap.getVolumeContainable(new int[]{5, 4, 4, 3, 4, 4}));
     }
 }
