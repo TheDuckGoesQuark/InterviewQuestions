0846de0787f2ee5ad442d8b4e4857feafd03139b
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-12 11:41:54 +0100

    nearly.

--- src/main/justifytext/JustifyText.java
+++ src/main/justifytext/JustifyText.java
@@ -1,6 +1,6 @@
 package main.justifytext;
 
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -30,7 +30,59 @@ import java.util.List;
 public class JustifyText {
 
     public static List<String> justifyText(List<String> words, int lineLength) {
-        return Collections.emptyList();
+        List<String> lines = new LinkedList<>();
+
+        int indexOfStartOfLine = 0;
+        int currentLengthCharacters = 0;
+        int numWords = 0;
+
+        for (int i = 0; i < words.size(); i++) {
+            String currentWord = words.get(i);
+
+            if ((currentLengthCharacters + numWords + currentWord.length()) > lineLength) {
+                // split previous line
+                addNewJustifiedLine(lines, words, indexOfStartOfLine, numWords, currentLengthCharacters, lineLength);
+
+                // start new line
+                currentLengthCharacters = 0;
+                numWords = 0;
+                indexOfStartOfLine = i;
+            }
+
+            // append this to line
+            currentLengthCharacters += currentWord.length();
+            numWords++;
+        }
+
+        if (numWords > 0)
+            addNewJustifiedLine(lines, words, indexOfStartOfLine, numWords, currentLengthCharacters, lineLength);
+
+        return lines;
+    }
+
+    private static void addNewJustifiedLine(List<String> lines, List<String> words, int startOfLine, int numWords, int currentLength, int lineLength) {
+        int insideSpaces = numWords - 1;
+        int charactersRemaining = lineLength - currentLength;
+        int numSpaces = charactersRemaining / insideSpaces;
+        int leftOver = charactersRemaining % insideSpaces;
+        int lastWord = startOfLine + numWords - 1;
+
+        final StringBuilder sb = new StringBuilder();
+
+        for (int i = startOfLine; i < lastWord; i++) {
+            sb.append(words.get(i));
+            sb.append(" ".repeat(numSpaces));
+
+            // Distribute remaining spaces, from left to right
+            if (leftOver > 0) {
+                sb.append(" ");
+                leftOver--;
+            }
+        }
+
+        sb.append(words.get(lastWord));
+
+        lines.add(sb.toString());
     }
 
 }
