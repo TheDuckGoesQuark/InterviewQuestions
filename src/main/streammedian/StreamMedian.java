7a33070426a0a681774fb28c3d99d91906b354f0
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-12 16:49:35 +0100

    add test case for stream median

---
+++ src/main/streammedian/StreamMedian.java
@@ -0,0 +1,25 @@
+package main.streammedian;
+
+/**
+ * Compute the running median of a sequence of numbers.
+ * That is, given a stream of numbers, print out the median of the list so far on each new element.
+ * <p>
+ * Recall that the median of an even-numbered list is the average of the two middle numbers.
+ * <p>
+ * For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should print out:
+ * <p>
+ * 2
+ * 1.5
+ * 2
+ * 3.5
+ * 2
+ * 2
+ * 2
+ */
+public class StreamMedian {
+
+    public static double[] getRollingMedian(double[] sequence) {
+        return null;
+    }
+
+}
