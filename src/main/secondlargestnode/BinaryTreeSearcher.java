8ff0a2d85c257c861b903b9858a6faaa592a6415
Jordan Mackie <jmackie97@hotmail.com>
2019-08-14 20:55:38 +0100

    finished find second largest

--- src/main/secondlargestnode/BinaryTreeSearcher.java
+++ src/main/secondlargestnode/BinaryTreeSearcher.java
@@ -11,8 +11,34 @@ public class BinaryTreeSearcher {
      * @param root root of binary tree
      * @return node with second largest value in tree
      */
-    public static BinarySearchNode findSecondLargestNode(BinarySearchNode root) {
-        return null;
+    public static BinarySearchNode findSecondLargestNode(BinarySearchNode<Integer> root) {
+        final BinarySearchNode[] maxes = new BinarySearchNode[2];
+        recursivelyFindSecondLargestNode(root, maxes);
+
+        return maxes[0];
     }
 
+    private static void recursivelyFindSecondLargestNode(BinarySearchNode<Integer> root, BinarySearchNode<Integer>[] maxes) {
+        insertIntoMaxes(root, maxes);
+
+        BinarySearchNode<Integer> left = root.getLeft();
+        if (left != null) recursivelyFindSecondLargestNode(left, maxes);
+        BinarySearchNode<Integer> right = root.getRight();
+        if (right != null) recursivelyFindSecondLargestNode(right, maxes);
+
+    }
+
+    private static void insertIntoMaxes(BinarySearchNode node, BinarySearchNode[] maxes) {
+        if (maxes[1] == null) maxes[1] = node;
+        else {
+            if (node.getValue().compareTo(maxes[1].getValue()) > 0) {
+                maxes[0] = maxes[1];
+                maxes[1] = node;
+            } else {
+                if (maxes[0] == null || node.getValue().compareTo(maxes[0].getValue()) > 0) {
+                    maxes[0] = node;
+                }
+            }
+        }
+    }
 }
