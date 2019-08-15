fe40b5c85041d4ff6848f327c801a17955757408
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-15 15:05:20 +0100

    finished powerset generation

--- src/main/powerset/PowerSet.java
+++ src/main/powerset/PowerSet.java
@@ -1,5 +1,6 @@
 package main.powerset;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -13,8 +14,29 @@ import java.util.Set;
  */
 public class PowerSet {
 
-    public static <E> E[][] generatePowerSet(E[] set) {
-        return null;
+    public static <T> Set<Set<T>> generatePowerSet(Set<T> set) {
+        final Set<Set<T>> result = new HashSet<>();
+
+        // add empty set
+        result.add(new HashSet<>());
+
+        // recursively create sets
+        recursivelyAddSets(set, result);
+
+        return result;
     }
 
+    private static <T> void recursivelyAddSets(Set<T> values, Set<Set<T>> result) {
+        if (values.size() == 0) return;
+
+        // add values currently in set
+        result.add(values);
+
+        // recursive call, after removing each element
+        for (T value : values) {
+            Set<T> copy = new HashSet<>(values);
+            copy.remove(value);
+            recursivelyAddSets(copy, result);
+        }
+    }
 }
