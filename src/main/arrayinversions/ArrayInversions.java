e40db9d8540e35ab3bcaf960e47adeddb8a630ae
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-16 15:48:37 +0100

    add tests for array inversions

---
+++ src/main/arrayinversions/ArrayInversions.java
@@ -0,0 +1,23 @@
+package main.arrayinversions;
+
+/**
+ * We can determine how "out of order" an array A is by counting the number of inversions it has.
+ * <p>
+ * Two elements A[i] and A[j] form an inversion if A[i] > A[j] but i < j.
+ * That is, a smaller element appears after a larger element.
+ * <p>
+ * Given an array, count the number of inversions it has. Do this faster than O(N^2) time.
+ * <p>
+ * You may assume each element in the array is distinct.
+ * <p>
+ * For example, a sorted list has zero inversions.
+ * The array [2, 4, 1, 3, 5] has three inversions: (2, 1), (4, 1), and (4, 3).
+ * The array [5, 4, 3, 2, 1] has ten inversions: every distinct pair forms an inversion.
+ */
+public class ArrayInversions {
+
+    public static int countNumInversions(int[] arr) {
+        return 0;
+    }
+
+}
