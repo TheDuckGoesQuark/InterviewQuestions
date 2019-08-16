36898a755d4956270f0445cc176dd5d0cfc4ad72
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-16 15:23:41 +0100

    set up for stack question

---
+++ src/main/stack/Stack.java
@@ -0,0 +1,34 @@
+package main.stack;
+
+/**
+ * Implement a stack that has the following methods:
+ * <p>
+ * push(val), which pushes an element onto the stack
+ * <p>
+ * pop(), which pops off and returns the topmost element of the stack.
+ * If there are no elements in the stack,
+ * then it should throw an error or return null.
+ * <p>
+ * max(), which returns the maximum value in the stack currently.
+ * If there are no elements in the stack,
+ * then it should throw an error or return null.
+ * <p>
+ * Each method should run in constant time.
+ */
+public class Stack<T extends Comparable<T>> {
+
+    public Stack() {
+    }
+
+    public void push(T val) {
+
+    }
+
+    public T pop() {
+        return null;
+    }
+
+    public T max() {
+        return null;
+    }
+}
---
+++ src/test/StackTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import main.stack.Stack;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StackTest {
+
+    @Test
+    public void push() {
+        Stack<Integer> stack = new Stack<>();
+    }
+
+    @Test
+    public void pop() {
+    }
+
+    @Test
+    public void max() {
+    }
+}
