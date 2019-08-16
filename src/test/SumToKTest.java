ae03a03548fc925afccbcbd68bc929fe350a985d
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-08-16 15:08:44 +0100

    sum to k passes

--- src/test/SumToKTest.java
+++ src/test/SumToKTest.java
@@ -9,6 +9,7 @@ import org.junit.runners.Parameterized.Parameters;
 import java.util.*;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 
 @RunWith(Parameterized.class)
@@ -16,9 +17,9 @@ public class SumToKTest {
 
     private final List<Integer> allowed;
     private final int target;
-    private final Set<Integer> expected;
+    private final List<Integer> expected;
 
-    public SumToKTest(List<Integer> allowed, int target, Set<Integer> expected) {
+    public SumToKTest(List<Integer> allowed, int target, List<Integer> expected) {
         this.allowed = allowed;
         this.target = target;
         this.expected = expected;
@@ -27,11 +28,11 @@ public class SumToKTest {
     @Parameters
     public static Collection<Object[]> data() {
         final Object[][] parameters = new Object[][]{
-                {Arrays.asList(12, 1, 61, 5, 9, 2), 24, new HashSet<>(Arrays.asList(12, 9, 2, 1))},
-                {Arrays.asList(3, 34, 4, 12, 5, 2), 9, new HashSet<>(Arrays.asList(4, 5))},
-                {Arrays.asList(1), 1, new HashSet<>(Arrays.asList(1))},
-                {Arrays.asList(1, 2), 3, new HashSet<>(Arrays.asList(1, 2))},
-                {Arrays.asList(1, 2, 15), 16, new HashSet<>(Arrays.asList(1, 15))},
+                {new LinkedList<>(Arrays.asList(12, 1, 61, 5, 9, 2)), 24, Arrays.asList(12, 9, 2, 1)},
+                {new LinkedList<>(Arrays.asList(3, 34, 4, 12, 5, 2)), 9, Arrays.asList(4, 5)},
+                {new LinkedList<>(Arrays.asList(1)), 1, Arrays.asList(1)},
+                {new LinkedList<>(Arrays.asList(1, 2)), 3, Arrays.asList(1, 2)},
+                {new LinkedList<>(Arrays.asList(1, 2, 15)), 16, Arrays.asList(1, 15)},
         };
 
         return Arrays.asList(parameters);
@@ -39,6 +40,10 @@ public class SumToKTest {
 
     @Test
     public void test() {
-        assertEquals(expected, SumToK.getSubsettThatAddsToK(allowed, target));
+        final Optional<List<Integer>> result = SumToK.getSubsetThatAddsToK(allowed, target);
+        assertTrue(result.isPresent());
+        Collections.sort(expected);
+        Collections.sort(result.get());
+        assertEquals(expected, result.get());
     }
 }
