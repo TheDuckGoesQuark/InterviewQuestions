927123ab00b5a711f798e81da5afae845d4e0b4e
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-09-01 10:28:11 +0100

    set up stock price profit array question

---
+++ src/main/stockpriceprofitarray/StockPriceProfit.java
@@ -0,0 +1,18 @@
+package main.stockpriceprofitarray;
+
+/**
+ * Given an array of numbers representing the stock prices of a company in chronological order write a
+ * function that calculates the maximum profit you could have made from buying and selling that stock once.
+ * <p>
+ * You must buy before you can sell it.
+ * <p>
+ * For example, given [9, 11, 8, 5 7, 10], you should return 5, since you could buy the stock at 5 dollars
+ * and sell it at 10 dollars.
+ */
+public class StockPriceProfit {
+
+    public static int mostProfitFromPrices(int[] prices) {
+        return 0;
+    }
+
+}
