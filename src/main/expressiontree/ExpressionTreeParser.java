8c235e7b480f56b219470a6d283da6d08c050d61
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-09-04 20:25:32 +0100

    set up expression parser q

---
+++ src/main/expressiontree/ExpressionTreeParser.java
@@ -0,0 +1,29 @@
+package main.expressiontree;
+
+/**
+ * Suppose an arithmetic expression is given as a binary tree.
+ * Each leaf is an integer and each internal node is one of '+', '−', '∗', or '/'.
+ * <p>
+ * Given the root to such a tree, write a function to evaluate it.
+ * <p>
+ * For example, given the following tree:
+ * <p>
+ * *
+ * / \
+ * +    +
+ * / \  / \
+ * 3  2  4  5
+ * You should return 45, as it is (3 + 2) * (4 + 5).
+ */
+public class ExpressionTreeParser {
+
+    public static final String PLUS = "+";
+    public static final String MINUS = "-";
+    public static final String MULTIPLY = "*";
+    public static final String DIVIDE = "/";
+
+    public int parse(ExpressionTree node) {
+        return 0;
+    }
+
+}
