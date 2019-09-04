1ad61de722c8ced0a51931985871662944098a1a
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-09-04 18:03:30 +0100

    set up test cases for b tree q

--- src/main/reconstructtree/TreeNode.java
+++ src/main/reconstructtree/TreeNode.java
@@ -1,5 +1,7 @@
 package main.reconstructtree;
 
+import java.util.Objects;
+
 public class TreeNode {
 
     private Object value;
@@ -26,4 +28,20 @@ public class TreeNode {
     public void setRight(TreeNode right) {
         this.right = right;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TreeNode treeNode = (TreeNode) o;
+        return Objects.equals(value, treeNode.value) &&
+                Objects.equals(left, treeNode.left) &&
+                Objects.equals(right, treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, left, right);
+    }
+
 }
