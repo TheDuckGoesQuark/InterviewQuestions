a886b8f7696fb7956e2e752c702c6f2d9d17961e
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-09-04 16:17:36 +0100

    setting up construction q

---
+++ src/main/reconstructtree/TreeNode.java
@@ -0,0 +1,29 @@
+package main.reconstructtree;
+
+public class TreeNode {
+
+    private Object value;
+
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(Object value) {
+        this.value = value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+}
---
+++ src/main/reconstructtree/TreeReconstructor.java
@@ -0,0 +1,31 @@
+package main.reconstructtree;
+
+// @formatter:off
+import com.sun.source.tree.Tree; /**
+ * Given pre-order and in-order traversals of a binary tree, write a function to reconstruct the tree.
+ *
+ * For example, given the following preorder traversal:
+ *
+ * [a, b, d, e, c, f, g]
+ *
+ * And the following inorder traversal:
+ *
+ * [d, b, e, a, f, c, g]
+ *
+ * You should return the following tree:
+ *
+ *     a
+ *    / \
+ *   b   c
+ *  / \ / \
+ * d  e f  g
+ */
+// @formatter:on
+public class TreeReconstructor {
+
+    public static TreeNode reconstructTree(String[] preOrder, String[] inOrder) {
+        return null;
+    }
+
+
+}
