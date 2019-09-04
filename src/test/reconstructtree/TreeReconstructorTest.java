5145bed6cda3af80216e15617245145c8263d976
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-09-04 16:21:16 +0100

    setting up tests for b tree q

--- src/test/reconstructtree/TreeReconstructorTest.java
+++ src/test/reconstructtree/TreeReconstructorTest.java
@@ -1,11 +1,13 @@
 package test.reconstructtree;
 
+import com.sun.source.tree.Tree;
 import main.reconstructtree.TreeNode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.parameterized.ParametersRunnerFactory;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.*;
@@ -24,17 +26,21 @@ public class TreeReconstructorTest {
         this.expected = expected;
     }
 
+    /*
+     * [a, b, d, e, c, f, g]
+     *
+     * And the following inorder traversal:
+     *
+     * [d, b, e, a, f, c, g]
+     */
     @Parameters
     public static Collection<Object[]> data() {
+        TreeNode root =
         final Object[][] data = new Object[][]{
-                {new int[]{13, 19, 13, 13}, 19},
-                {new int[]{2, 3, 3, 3}, 2},
-                {new int[]{3, 2, 3, 3}, 2},
-                {new int[]{3, 3, 2, 3}, 2},
-                {new int[]{3, 3, 3, 2}, 2},
-                {new int[]{1, 2, 3, 1, 2, 1, 2}, 3},
-                {new int[]{1, 2, 2, 2, 3, 3, 3}, 1},
-                {new int[]{6, 1, 3, 3, 3, 6, 6}, 1}
+                {
+                        new String[]{"a", "b", "d", "e", "c", "f", "g"},
+                        new String[]{"d", "b", "e", "a", "f", "c", "g"},
+                }
         };
 
         return Arrays.asList(data);
