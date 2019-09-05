585428ae3e191e2df683193a00882db144093829
TheDuckGoesQuark <jmackie97@hotmail.com>
2019-09-05 17:34:50 +0100

    added test case for sudoku

--- src/test/sudoku/SudokuSolverTest.java
+++ src/test/sudoku/SudokuSolverTest.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.*;
+import static org.junit.runners.Parameterized.*;
 
 @RunWith(Parameterized.class)
 public class SudokuSolverTest {
@@ -21,30 +22,31 @@ public class SudokuSolverTest {
         this.expected = expected;
     }
 
-    @Parameterized.Parameters
+    @Parameters
     public static Collection<Object[]> data() {
         Object[][] data = new Object[][]{
                 {
-                        new int[][]{{8, 6, 0, 0, 2, 0, 0, 0, 0},
-                                {0, 0, 0, 7, 0, 0, 0, 5, 9},
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                {0, 0, 0, 0, 6, 0, 8, 0, 0},
-                                {0, 4, 0, 0, 0, 0, 0, 0, 0},
-                                {0, 0, 5, 3, 0, 0, 0, 0, 7},
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                {0, 2, 0, 0, 0, 0, 6, 0, 0},
-                                {0, 0, 7, 5, 0, 9, 0, 0, 0}
+                        new int[][]{
+                                {0, 0, 0, 0, 0, 4, 0, 9, 0},
+                                {8, 0, 2, 9, 7, 0, 0, 0, 0},
+                                {9, 0, 1, 2, 0, 0, 3, 0, 0},
+                                {0, 0, 0, 0, 4, 9, 1, 5, 7},
+                                {0, 1, 3, 0, 5, 0, 9, 2, 0},
+                                {5, 7, 9, 1, 2, 0, 0, 0, 0},
+                                {0, 0, 7, 0, 0, 2, 6, 0, 3},
+                                {0, 0, 0, 0, 3, 8, 2, 0, 5},
+                                {0, 2, 0, 5, 0, 0, 0, 0, 0}
                         },
-                        // get answer
-                        new int[][]{{8, 6, 0, 0, 2, 0, 0, 0, 0},
-                                {0, 0, 0, 7, 0, 0, 0, 5, 9},
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                {0, 0, 0, 0, 6, 0, 8, 0, 0},
-                                {0, 4, 0, 0, 0, 0, 0, 0, 0},
-                                {0, 0, 5, 3, 0, 0, 0, 0, 7},
-                                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                                {0, 2, 0, 0, 0, 0, 6, 0, 0},
-                                {0, 0, 7, 5, 0, 9, 0, 0, 0}
+                        new int[][]{
+                                {7, 3, 5, 6, 1, 4, 8, 9, 2},
+                                {8, 4, 2, 9, 7, 5, 5, 6, 1},
+                                {9, 6, 1, 2, 8, 3, 3, 7, 4},
+                                {2, 8, 6, 3, 4, 9, 1, 5, 7},
+                                {4, 1, 3, 8, 5, 7, 9, 2, 6},
+                                {5, 7, 9, 1, 2, 6, 4, 3, 8},
+                                {1, 5, 7, 4, 9, 2, 6, 8, 3},
+                                {6, 9, 4, 7, 3, 8, 2, 1, 5},
+                                {3, 2, 8, 5, 6, 1, 7, 4, 9}
                         },
                 }
         };
@@ -55,6 +57,9 @@ public class SudokuSolverTest {
     @Test
     public void name() {
         SudokuSolver.solve(input);
-        assertArrayEquals(expected, input);
+
+        for (int i = 0; i < input.length; i++) {
+            assertArrayEquals(expected[i], input[i]);
+        }
     }
 }
