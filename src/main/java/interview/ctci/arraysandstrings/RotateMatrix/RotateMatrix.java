d7f81594afa940158ec83090b1aabaf0dbcee063
Jordan Mackie <jordan@gravitysketch.com>
2023-11-06 17:29:03 -0500

    rotate matrix O(n)

--- src/main/java/interview/ctci/arraysandstrings/RotateMatrix/RotateMatrix.java
+++ src/main/java/interview/ctci/arraysandstrings/RotateMatrix/RotateMatrix.java
@@ -1,8 +1,21 @@
 package interview.ctci.arraysandstrings.RotateMatrix;
 
 /**
- *
+ * Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
+ * write a method to rotate the image by 90 degrees. In place?
  */
 public class RotateMatrix {
+    public static int[][] rotateImage(int[][] imageArr) {
+        if (imageArr.length == 0) return imageArr;
 
+        var output = new int[imageArr.length][imageArr[0].length];
+        for (int i = 0; i < imageArr.length; i++) {
+            for (int j = 0; j < imageArr[i].length; j++) {
+                var rotatedJ = imageArr.length - 1 - i;
+                output[j][rotatedJ] = imageArr[i][j];
+            }
+        }
+
+        return output;
+    }
 }
--- src/test/java/interview/ctci/arraysandstrings/RotateMatrix/RotateMatrixTest.java
+++ src/test/java/interview/ctci/arraysandstrings/RotateMatrix/RotateMatrixTest.java
@@ -1,5 +1,6 @@
 package interview.ctci.arraysandstrings.RotateMatrix;
 
+import interview.Util;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -8,21 +9,45 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class RotateMatrixTest {
 
-    public RotateMatrixTest() {
+    private final int[][] input;
+    private final int[][] expectedOutput;
+
+    public RotateMatrixTest(int[][] input, int[][] expectedOutput) {
+        this.input = input;
+        this.expectedOutput = expectedOutput;
     }
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         final var inputs = new Object[][]{
+                {
+                        new int[][]{{0}},
+                        new int[][]{{0}}
+                },
+                {
+                        new int[][]{{0, 1}, {2, 3}},
+                        new int[][]{{2, 0}, {3, 1}}
+                },
+                {
+                        new int[][]{{0, 1, 2}, {3, 4, 5}, {6, 7, 8}},
+                        new int[][]{{6, 3, 0}, {7, 4, 1}, {8, 5, 2}},
+                },
+                {
+                        new int[][]{{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}},
+                        new int[][]{{12, 8, 4, 0}, {13, 9, 5, 1}, {14, 10, 6, 2}, {15, 11, 7, 3}}
+                },
         };
         return Arrays.asList(inputs);
     }
 
     @Test
     public void test() {
+        var output = RotateMatrix.rotateImage(input);
+        assertArrayEquals(Util.twoDimensionalArrayToString(input), expectedOutput, output);
     }
 }
