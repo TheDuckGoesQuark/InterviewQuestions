33698825e6aa3ea157f635d8244638b6982b0a57
Jordan Mackie <jordan@gravitysketch.com>
2023-11-06 18:31:55 -0500

    in place array rotation!

--- src/main/java/interview/ctci/arraysandstrings/RotateMatrix/RotateMatrix.java
+++ src/main/java/interview/ctci/arraysandstrings/RotateMatrix/RotateMatrix.java
@@ -8,14 +8,31 @@ public class RotateMatrix {
     public static int[][] rotateImage(int[][] imageArr) {
         if (imageArr.length == 0) return imageArr;
 
-        var output = new int[imageArr.length][imageArr[0].length];
-        for (int i = 0; i < imageArr.length; i++) {
-            for (int j = 0; j < imageArr[i].length; j++) {
-                var rotatedJ = imageArr.length - 1 - i;
-                output[j][rotatedJ] = imageArr[i][j];
-            }
+        var numLayers = Math.ceil(((double) imageArr.length) / 2);
+        for (int i = 0; i < numLayers; i++) {
+            rotateLayer(i, imageArr);
         }
 
-        return output;
+        return imageArr;
+    }
+
+    private static void rotateLayer(int layer, int[][] imageArr) {
+        var layerWidth = imageArr.length - (layer * 2);
+
+        var temp = new int[layerWidth - 1];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = imageArr[layer][layer + i];
+        }
+
+        for (int i = 0; i < layerWidth - 1; i++) {
+            // move left to top
+            imageArr[layer][layer + i] = imageArr[imageArr.length - layer - i - 1][layer];
+            // move bottom to left
+            imageArr[imageArr.length - layer - i - 1][layer] = imageArr[imageArr.length - layer - 1][imageArr.length - layer - 1 - i];
+            // move right to bottom
+            imageArr[imageArr.length - layer - 1][imageArr.length - layer - 1 - i] = imageArr[layer + i][imageArr.length - layer - 1];
+            // move temp top to right
+            imageArr[layer + i][imageArr.length - layer - 1] = temp[i];
+        }
     }
 }
